9ae8c9142f04506be319730a09ae33a10e150bf6
Eero Vanha-perttula <evanha-p@c3r4p2.hive.fi>
2022-07-06 16:17:12 +0300

    Added comments to fdf.h plus two new structs, one for points and one to boolean struct

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 16:54:58 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/06/29 19:24:48 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/07/06 16:15:40 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@
 # include "minilibx/mlx.h"
 # include <stdlib.h>
 # include <math.h>
-# include <stdio.h> /* DELETE THIS BEFORE COMMITTING!!! */
+
+/* Stores all mlx related data */
 
 typedef struct s_mlx
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 }	t_mlx;
+
+/* Stores properties of a line */
 
 typedef struct s_line
 {
@@ -32,6 +35,26 @@ typedef struct s_line
 	int		end_y;
 	int		color;
 }	t_line;
+
+/* Stores properties of a single point */
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+	int		height;
+	t_point	*next;
+}	t_point;
+
+/* Created a custom boolian so we can improve readability
+ * by returning false/true instead of 0 or 1.
+ */
+
+typedef enum e_bool
+{
+	false,
+	true
+}	t_bool;
 
 int		draw_line(t_mlx *mlx, t_line *line);
 
