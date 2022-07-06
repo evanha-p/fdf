b2520a98f85922e5dde12ae69213b862870dd5cd
Eero Vanha-perttula <evanha-p@c3r4p2.hive.fi>
2022-07-06 14:31:58 +0300

    added comments to draw_line function in draw.c

--- draw.c
+++ draw.c
@@ -6,11 +6,47 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 16:53:43 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/06/29 19:19:07 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/07/06 14:31:30 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+/* 
+ * Draw_line function uses tan a to calculate the location for y.
+ * Since we can form a right triangle from one point to next
+ * we can use trigonometric functions to calculate value of y
+ * everytime we increase the value of x by 1.
+ * The right triangle is formed with:
+ * - line between start and finish being the hypotenuse
+ * - Difference from x at start to finish and from y at start to finish
+ *   being the cathetuses
+ * 
+ * Example:
+ *                    finish
+ *                       x
+ *                   .   |
+ *                .      |
+ *            .          | y
+ *         .             |
+ *     .                 |
+ *   x - - - - - - - - - /
+ * start        x
+ *
+ * First we calcute the length of x and y (the sides of the triangle) by
+ * substracting the x and y values at the end by the x and y values at the start.
+ *
+ * length of x = x coordinate  at end - x coordinate  at beginning (same for the y value)
+ *
+ * With that we calculate tan a.
+ *
+ * tan a = y/x
+ *
+ * When we have solved tan a we can use it to calculate the values of y
+ * when value of x changes.
+ *
+ * y = tan a * x
+ */
 
 int	draw_line(t_mlx *mlx, t_line *line)
 {
