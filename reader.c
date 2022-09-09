b1dd49b5a31e040f5ce9e8464b44d468750b36d0
Eero Vanha-perttula <evanha-p@c1r6p14.hive.fi>
2022-09-09 14:52:22 +0300

    added comments to reader.c

--- reader.c
+++ reader.c
@@ -6,12 +6,28 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:45:08 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/09/08 17:16:33 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/09/09 14:52:02 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+/*
+Static helper function for reader. Stores the values to a linked list.
+The function receives a row (stored in variable line) from the original
+file and splits it to character string using space (' ') as a delimeter using
+ft_strsplit. The strings are stored in variable values.
+
+After splitting we go through the values and use ft_atoi
+to get the height and store it to point->z.
+
+The x-value is based on the string we are currently on.
+So the first string gets the x-value of 0 and so on.
+
+The y-value is base on what row we are currently reading.
+This information is sent by the callee function (reader) and
+stored in v->y_coord.
+*/
 static	t_point	*set_values(t_point *point, char *line, t_var *v)
 {
 	int		i;
@@ -33,6 +49,60 @@ static	t_point	*set_values(t_point *point, char *line, t_var *v)
 	return (point);
 }
 
+/*
+Main function that calls the helper function.
+We use special type of variable t_var *v to store multiple variables
+such as ret, fd and so on (see the struct in fdf.h for the full list).
+We use a helper function "initialize variables" (located in utils.c)
+to initialize all the variables to 0.
+
+Function errors is used throughout the program and it gets called if any
+errors are detected. Based on the string used as an argument errors function
+can detect what kind of an error happened and print corresponding error message.
+
+We read the file and check that it only contains digits or spaces with
+check_line -function. Then we store the values of the points to a
+linked list by using function set_values.
+Note: we use varibale v->y_coord to keep track on the row we are on.
+
+Finally we check the all the nodes (the points) using check_nodes function.
+The function detects if no points are stored (file was empty/contained only
+spaces) and makes sure every line has the equal amount of points.
+For example:
+
+4 5 3 6 3 4
+1 3 3 4 5 2 1
+3 5 2 1 0 0 
+
+Above the second row has more points stored than on the first or third
+row. This would be detected by the check_nodes -function.
+
+We must do the check since our program cannot detect "holes" in the map.
+So if one would want the program to draw a box they could give a file
+like:
+
+0 0 0
+0   0
+0 0 0
+
+the program would not remember where the empty spot was. As a result the program
+would read the file as if it would have been like this:
+
+0 0 0
+0 0 
+0 0 0
+
+We don't want this kind of behaviour so I chose to only accept files where
+every line has equal amount of points. For example:
+
+0 0 0
+0 0 0
+0 0 0
+
+is a valid file.
+
+After the last check we return temp to the callee function.
+*/
 t_point	*reader(char *argv)
 {
 	t_var	*v;
