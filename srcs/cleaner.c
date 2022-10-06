c64428b3abe272f79e0d369a192ab39e27ce3722
Eero Vanha-perttula <evanha-p@c2r6p12.hive.fi>
2022-10-06 17:28:29 +0300

    added comments to the new file cleaner.c

--- srcs/cleaner.c
+++ srcs/cleaner.c
@@ -6,11 +6,17 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 15:39:56 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/10/06 17:03:43 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/10/06 17:27:10 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+/*
+Function goes through the linked list, sets all data to 0 and
+frees all the nodes. It also sets mlx and v data to 0
+and frees the pointers.
+*/
 
 void	free_pointers(t_ptrs *pointers)
 {
@@ -25,6 +31,7 @@ void	free_pointers(t_ptrs *pointers)
 	}
 	ft_bzero(pointers->mlx, sizeof(t_mlx *));
 	free(pointers->mlx);
+	ft_bzero(pointers->v, sizeof(t_mlx *));
 	free(pointers->v);
 	free(pointers);
 }
